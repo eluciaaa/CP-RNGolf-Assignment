5ffdbcad46449ebad55506fa7a52b76890fc4be3
Elucia <abbyc9113@gmail.com>
2026-01-30 20:54:11 -0800

    initial commit

---
+++ src/Play.js
@@ -0,0 +1,107 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene')
+    }
+
+    init() {
+        // useful variables
+        this.SHOT_VELOCITY_X_MIN = 500
+        this.SHOT_VELOCITY_X_MAX = 900
+        this.SHOT_VELOCITY_Y_MIN = 700
+        this.SHOT_VELOCITY_Y_MAX = 1100
+        this.shotCount = 0
+        this.scoreCount = 0
+        this.successPercent = 0
+    }
+
+    preload() {
+        this.load.path = './assets/img/'
+        this.load.image('grass', 'grass.jpg')
+        this.load.image('cup', 'cup.jpg')
+        this.load.image('ball', 'ball.png')
+        this.load.image('wall', 'wall.png')
+        this.load.image('oneway', 'one_way_wall.png')
+    }
+
+    create() {
+        // add background grass
+        this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
+
+        // add onscreen counters
+        this.shotCountText = this.add.text(5, 5, 'Shots: ' + this.shotCount, { fontSize: '30px', fill: '#fff' }).setDepth(1)
+        this.scoreCountText = this.add.text(200, 5, 'Score: ' + this.scoreCount, { fontSize: '30px', fill: '#fff' }).setDepth(1)
+        this.successPercentText = this.add.text(380, 5, 'Success %: ' + this.scoreCount + '%', { fontSize: '30px', fill: '#fff' }).setDepth(1)
+
+        // add cup
+        this.cup = this.physics.add.sprite(width / 2, height / 10, 'cup')
+        this.cup.body.setCircle(this.cup.width / 4)
+        this.cup.body.setOffset(this.cup.width / 4)
+        this.cup.body.setImmovable(true)
+        
+        // add ball
+        this.ball = this.physics.add.sprite(width / 2, height - height / 10, 'ball')
+        this.ball.body.setCircle(this.ball.width / 2)
+        this.ball.body.setCollideWorldBounds(true)
+        this.ball.body.setBounce(0.5)
+        this.ball.body.setDamping(true).setDrag(0.5)
+
+        // add walls
+        let wallA = this.physics.add.sprite(0, height / 4, 'wall')
+        wallA.setX(Phaser.Math.Between(0 + wallA.width / 2, width - wallA.width / 2))
+        wallA.body.setImmovable(true)
+        wallA.body.setCollideWorldBounds(true)
+        wallA.body.setVelocityX(250)
+        wallA.body.setBounce(1)
+
+        let wallB = this.physics.add.sprite(0, height / 2, 'wall')
+        wallB.setX(Phaser.Math.Between(0 + wallB.width / 2, width - wallB.width / 2))
+        wallB.body.setImmovable(true)
+
+        this.walls = this.add.group([wallA, wallB])
+
+        // add one-way
+        this.oneWay = this.physics.add.sprite(width / 2, height / 4 * 3, 'oneway')
+        this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width / 2, width - this.oneWay.width / 2))
+        this.oneWay.body.setImmovable(true)
+        this.oneWay.body.checkCollision.down = false
+
+
+        // add pointer input
+        this.input.on('pointerdown', (pointer) => {
+            this.shotCount ++
+            this.shotCountText.setText('Shots: ' + this.shotCount)
+            let shotDirectionY = pointer.y <= this.ball.y ? 1 : -1
+            let shotDirectionX = pointer.x <= this.ball.x ? 1 : -1
+            this.ball.body.setVelocityX(Phaser.Math.Between(this.SHOT_VELOCITY_X_MIN, this.SHOT_VELOCITY_X_MAX) * shotDirectionX)
+            this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirectionY)
+        })
+
+        // cup/ball collision
+        this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
+            this.scoreCount ++
+            this.scoreCountText.setText('Score: ' + this.scoreCount)
+            this.successPercent = this.scoreCount / this.shotCount
+            this.successPercentText.setText('Success %: ' + Math.round(this.successPercent * 100) + '%')
+            ball.setPosition(width / 2, height - height / 10)
+            ball.body.stop()
+        })
+
+        // ball/wall collision
+        this.physics.add.collider(this.ball, this.walls)
+
+        // ball/one-way collision
+        this.physics.add.collider(this.ball, this.oneWay)
+    }
+
+    update() {
+
+    }
+}
+/*
+CODE CHALLENGE
+Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
+[✅] Add ball reset logic on successful shot
+[✅] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[✅] Make one obstacle move left/right and bounce against screen edges
+[✅] Create and display shot counter, score, and successful shot percentage
+*/
